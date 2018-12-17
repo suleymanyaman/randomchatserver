7cedce3690ea932d7772f26dd53116f9cf19000f
Süleyman Yaman <33877588+suleymanyaman@users.noreply.github.com>
2018-12-17 19:15:20 +0300

    Update login_screen.py

--- login_screen.py
+++ login_screen.py
@@ -3,40 +3,37 @@ import MySQLdb
 from tkinter import messagebox
 
 
-def screen():
-    top = tkinter.Tk()
-    top.geometry("500x500")
 
-    db = MySQLdb.connect(host="localhost", user="root", passwd="", db="members")
-    cursor = db.cursor()
+top = tkinter.Tk()
+top.geometry("500x500")
 
-    label = tkinter.Label(top, text="Nickname:")
-    label.place(x=140, y=150)
+db = MySQLdb.connect(host="localhost", user="root", passwd="", db="members")
+cursor = db.cursor()
 
-    user_id = tkinter.Entry(top)
-    user_id.place(x=210, y=150)
+label = tkinter.Label(top, text="Nickname:")
+label.place(x=140, y=150)
 
-    label_2 = tkinter.Label(top, text="Password:")
-    label_2.place(x=140, y=200)
+user_id = tkinter.Entry(top)
+user_id.place(x=210, y=150)
 
-    password = tkinter.Entry(top, show="*")
-    password.place(x=210, y=200)
+label_2 = tkinter.Label(top, text="Password:")
+label_2.place(x=140, y=200)
 
-    def auth_login(event=None):
-        id = user_id.get()
-        pswd = password.get()
-        cursor.execute(
-            "SELECT nickname, password FROM chatmembers WHERE nickname='{}' AND password='{}' ".format(id, pswd))
-        if cursor.fetchone():
-            top.destroy()
+password = tkinter.Entry(top, show="*")
+password.place(x=210, y=200)
 
-
-        else:
-            messagebox.showerror("Wrong credentials", "Either nickname or password is wrong. Please try again.")
-
-    login_button = tkinter.Button(top, text="Log In", command=auth_login, height=2, width=20)
-    login_button.place(x=195, y=250)
-    top.mainloop()
+def auth_login(event=None):
+    id = user_id.get()
+    pswd = password.get()
+    cursor.execute(
+        "SELECT nickname, password FROM chatmembers WHERE nickname='{}' AND password='{}' ".format(id, pswd))
+    if cursor.fetchone():
+        top.destroy()
 
 
-screen()
+    else:
+        messagebox.showerror("Wrong credentials", "Either nickname or password is wrong. Please try again.")
+
+login_button = tkinter.Button(top, text="Log In", command=auth_login, height=2, width=20)
+login_button.place(x=195, y=250)
+top.mainloop()
