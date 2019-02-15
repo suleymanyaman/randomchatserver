8bea22ca2f5b5ed6374c993fe490c4f9b7cdc117
Süleyman Yaman <33877588+suleymanyaman@users.noreply.github.com>
2019-02-15 23:13:45 +0300

    Update chatserver.py

--- chatserver.py
+++ chatserver.py
@@ -3,7 +3,6 @@ from threading import Thread
 import random
 
 
-
 def get_key(dict, value):
     return list(dict.keys())[list(dict.values()).index(value)]
 
@@ -45,14 +44,14 @@ def handle_client(client):
             if client_id in list(match.keys())+list(match.values()):
                 try:
                     del match[client_id]
-                    get_key(clients, target_client).send("[FromServer]Your partner has left the chat :(".encode())
+                    get_key(clients, target_client).send("Your partner has left the chat :(".encode())
                     status[target_client]="AVAILABLE"
 
 
 
                 except KeyError:
                     del match[get_key(match, client_id)]
-                    get_key(clients, target_client).send("[FromServer]Your partner has left the chat :(".encode())
+                    get_key(clients, target_client).send("Your partner has left the chat :(".encode())
                     status[target_client] = "AVAILABLE"
 
             target_client = random.choice(list(clients.values()))
@@ -60,19 +59,19 @@ def handle_client(client):
                 match[client_id] = target_client
                 status[client_id] = "FULL"
                 status[target_client] = "FULL"
-                client.send("[FromServer]You have been connected to someone!".encode())
+                client.send("You have been connected to someone!".encode())
                 get_key(clients,target_client).send(
-                    "[FromServer]You have been connected to someone!".encode())
+                    "You have been connected to someone!".encode())
 
 
             else:
-                client.send("[FromServer]We couldn't find a match for you, try again".encode())
+                client.send("We couldn't find a match for you, try again".encode())
                 continue
 
         elif msg == "4t7w!z%C":
             client.close()
             try:
-                get_key(clients, target_client).send("[FromServer]Your partner has left the chat :(".encode())
+                get_key(clients, target_client).send("Your partner has left the chat :(".encode())
                 status[target_client] = "AVAILABLE"
             finally:
                 del clients[client]
